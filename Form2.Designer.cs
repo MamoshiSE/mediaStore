4ddc4ef0d9e329dad35ca5568310801d9ef9bafe
MamoshiSE <33344169+MamoshiSE@users.noreply.github.com>
2020-04-13 23:24:30 +0200

    table + update quantity of products

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -29,19 +29,21 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.productName = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.titleStoremenScreen = new System.Windows.Forms.TextBox();
-            this.productPrice = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.productId = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
+            this.addProduct = new System.Windows.Forms.Button();
             this.productMedia = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.productQuantity = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.addProduct = new System.Windows.Forms.Button();
+            this.productId = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.productPrice = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.productName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.titleStoremenScreen = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -59,10 +61,90 @@
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.titleStoremenScreen);
             this.panel1.ForeColor = System.Drawing.SystemColors.Window;
-            this.panel1.Location = new System.Drawing.Point(164, 253);
+            this.panel1.Location = new System.Drawing.Point(164, 294);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(563, 241);
             this.panel1.TabIndex = 0;
+            // 
+            // addProduct
+            // 
+            this.addProduct.BackColor = System.Drawing.SystemColors.Highlight;
+            this.addProduct.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.addProduct.Location = new System.Drawing.Point(239, 174);
+            this.addProduct.Name = "addProduct";
+            this.addProduct.Size = new System.Drawing.Size(75, 32);
+            this.addProduct.TabIndex = 3;
+            this.addProduct.Text = "Add";
+            this.addProduct.UseVisualStyleBackColor = false;
+            this.addProduct.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // productMedia
+            // 
+            this.productMedia.Location = new System.Drawing.Point(141, 124);
+            this.productMedia.Name = "productMedia";
+            this.productMedia.Size = new System.Drawing.Size(117, 20);
+            this.productMedia.TabIndex = 19;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(89, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 16);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Media:";
+            // 
+            // productQuantity
+            // 
+            this.productQuantity.Location = new System.Drawing.Point(367, 124);
+            this.productQuantity.Name = "productQuantity";
+            this.productQuantity.Size = new System.Drawing.Size(73, 20);
+            this.productQuantity.TabIndex = 17;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(302, 126);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 16);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Quantity:";
+            // 
+            // productId
+            // 
+            this.productId.Location = new System.Drawing.Point(406, 72);
+            this.productId.Name = "productId";
+            this.productId.Size = new System.Drawing.Size(117, 20);
+            this.productId.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(326, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 16);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Product ID:";
+            // 
+            // productPrice
+            // 
+            this.productPrice.Location = new System.Drawing.Point(239, 72);
+            this.productPrice.Name = "productPrice";
+            this.productPrice.Size = new System.Drawing.Size(73, 20);
+            this.productPrice.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(195, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 16);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Price:";
             // 
             // productName
             // 
@@ -101,85 +183,16 @@
             this.titleStoremenScreen.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.titleStoremenScreen.TextChanged += new System.EventHandler(this.titleMainScreen_TextChanged);
             // 
-            // productPrice
+            // dataGridView1
             // 
-            this.productPrice.Location = new System.Drawing.Point(239, 72);
-            this.productPrice.Name = "productPrice";
-            this.productPrice.Size = new System.Drawing.Size(73, 20);
-            this.productPrice.TabIndex = 13;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(195, 73);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(48, 16);
-            this.label1.TabIndex = 12;
-            this.label1.Text = "Price:";
-            // 
-            // productId
-            // 
-            this.productId.Location = new System.Drawing.Point(406, 72);
-            this.productId.Name = "productId";
-            this.productId.Size = new System.Drawing.Size(117, 20);
-            this.productId.TabIndex = 15;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(326, 74);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(84, 16);
-            this.label3.TabIndex = 14;
-            this.label3.Text = "Product ID:";
-            // 
-            // productMedia
-            // 
-            this.productMedia.Location = new System.Drawing.Point(141, 124);
-            this.productMedia.Name = "productMedia";
-            this.productMedia.Size = new System.Drawing.Size(117, 20);
-            this.productMedia.TabIndex = 19;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(89, 125);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(55, 16);
-            this.label4.TabIndex = 18;
-            this.label4.Text = "Media:";
-            // 
-            // productQuantity
-            // 
-            this.productQuantity.Location = new System.Drawing.Point(367, 124);
-            this.productQuantity.Name = "productQuantity";
-            this.productQuantity.Size = new System.Drawing.Size(73, 20);
-            this.productQuantity.TabIndex = 17;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(302, 126);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(68, 16);
-            this.label5.TabIndex = 16;
-            this.label5.Text = "Quantity:";
-            // 
-            // addProduct
-            // 
-            this.addProduct.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.addProduct.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.addProduct.Location = new System.Drawing.Point(239, 174);
-            this.addProduct.Name = "addProduct";
-            this.addProduct.Size = new System.Drawing.Size(75, 32);
-            this.addProduct.TabIndex = 3;
-            this.addProduct.Text = "Add";
-            this.addProduct.UseVisualStyleBackColor = false;
-            this.addProduct.Click += new System.EventHandler(this.button1_Click);
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(164, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(563, 240);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
             // 
             // Form2
             // 
@@ -187,12 +200,14 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDark;
             this.ClientSize = new System.Drawing.Size(913, 547);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.panel1);
             this.Name = "Form2";
             this.Text = "Form2";
             this.Load += new System.EventHandler(this.Form2_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -212,5 +227,6 @@
         private System.Windows.Forms.TextBox productPrice;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button addProduct;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- Form2.cs
+++ Form2.cs
@@ -20,29 +20,39 @@ namespace mediaStore
             InitializeComponent();
             this.BackColor = ColorTranslator.FromHtml("#0f1923");
             titleStoremenScreen.BackColor = ColorTranslator.FromHtml("#0f1923");
-           
+            dataGridView1.BackgroundColor = ColorTranslator.FromHtml("#0f1923");
+
 
         }
 
         private void Form2_Load(object sender, EventArgs e)
         {
-           // read CSV file and store each line in array
-            string[] lines = System.IO.File.ReadAllLines("products.csv");
-            for (int i = 0; i < lines.Length; i++)
+           
+            // read CSV file and store each line in array
+            if (File.Exists("products.csv"))
             {
-                // split each value in a line with ","
-                var values = lines[i].Split(',');
-                string name = values[0];
-                double price = double.Parse(values[1]);
-                int id = Int32.Parse(values[2]);
-                string media = values[3];
-                int quantity = Int32.Parse(values[4]);
-                Product product = new Product(name, price, id, media, quantity);
-                productsList.Add(new Product(name, price, id, media, quantity));
-                System.Console.WriteLine(product.Name);
+                string[] lines = System.IO.File.ReadAllLines("products.csv");
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    // split each value in a line with ","
+                    var values = lines[i].Split(',');
+                    string name = values[0];
+                    double price = double.Parse(values[1]);
+                    int id = Int32.Parse(values[2]);
+                    string media = values[3];
+                    int quantity = Int32.Parse(values[4]);
+                    Product product = new Product(name, price, id, media, quantity);
+                    productsList.Add(new Product(name, price, id, media, quantity));
+
+                    System.Console.WriteLine(product.Name);
+                }
+                dataGridView1.DataSource = productsList;
+                dataGridView1.Columns[0].ReadOnly = true;
+                dataGridView1.Columns[1].ReadOnly = true;
+                dataGridView1.Columns[2].ReadOnly = true;
+                dataGridView1.Columns[3].ReadOnly = true;
             }
-               
-          
+
         }
 
         private void titleMainScreen_TextChanged(object sender, EventArgs e)
@@ -78,6 +88,11 @@ namespace mediaStore
                 Product product = new Product(name, price, id, media, quantity);
                 productsList.Add(new Product(name, price, id, media, quantity));
                 writeToCSV(product);
+                // reset datagridview so it updates in realtime and reassign
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = productsList;
+               
+               
             }
         }
 
@@ -95,7 +110,24 @@ namespace mediaStore
             }
             }
 
-          
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
+
+        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            var sb = new StringBuilder();
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                var cells = row.Cells.Cast<DataGridViewCell>();
+                sb.AppendLine(string.Join(",", cells.Select(cell =>  cell.Value ).ToArray()));
+            }
+            
+            System.IO.File.WriteAllText("products.csv", sb.ToString());
+            
+        }
+    }
     }
 
